3aba094e11c6424a0213123b8513f8661cdd3a53
Guilherme <guilhermekbaccin@gmail.com>
2026-03-29 21:56:38 -0300

    Initial commit

---
+++ exercicios/numperfeito.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void numperf(int n){
+int i;
+int div;
+for(i=0; i<n; i++){
+if(i%n==0){
+div =i;
+if(i==n){
+    printf("%d", n);
+}
+
+}
+
+}
+
+
+
+}
+int main(){
+ 
+ 
+ 
+ 
+    numperf(98);
+}
